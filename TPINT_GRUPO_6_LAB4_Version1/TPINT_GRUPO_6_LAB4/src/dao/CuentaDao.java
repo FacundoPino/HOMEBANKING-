05d6a2f40ffdf166e1c50eebde95c1af0322c6d5
Pedro Quiros <pedro.quiros@alumnos.frgp.utn.edu.ar>
2024-11-19 22:01:38 -0300

    Cambio

--- TPINT_GRUPO_6_LAB4_Version1/TPINT_GRUPO_6_LAB4/src/dao/CuentaDao.java
+++ TPINT_GRUPO_6_LAB4_Version1/TPINT_GRUPO_6_LAB4/src/dao/CuentaDao.java
@@ -6,6 +6,7 @@ import Entidades.Cliente;
 import Entidades.Cuenta;
 
 public interface CuentaDao {
+	//CuentaDao
 	public boolean insertCuenta(Cuenta cuenta);
 	public boolean EliminarCuenta(int id);
 	public ArrayList<Cuenta> ListarCuenta(int DNI);
